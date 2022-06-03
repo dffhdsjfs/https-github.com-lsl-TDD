6627e2d7e58e8c040cde38a15905931f8ce4cadd
wuyuxun <1779817269@qq.com>
2022-06-03 15:17:46 +0800

    first steps of FT for layout + stying

--- suplerlists/lists/views.py
+++ suplerlists/lists/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect, render
 from lists.models import Item, List
 
 def home_page(request):
+    # if request.method == 'POST':
+    #     Item.objects.create(text = request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request, list_id):

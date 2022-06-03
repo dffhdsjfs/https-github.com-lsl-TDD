d1fe2b3eadbd20d65de3351c554e07a7c064215b
wuyuxun <1779817269@qq.com>
2022-06-03 11:55:11 +0800

    URLS for forms x2,moved code in views + tests,new URL

--- suplerlists/suplerlists/urls.py
+++ suplerlists/suplerlists/urls.py
@@ -19,4 +19,6 @@ from  django.urls import path
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path ('',views.home_page,name='home'),
+    path('lists/the-only-list-in-the-world/',views.view_list,name="view_list"),
+    path('lists/new',views.new_list,name = 'new_list'),
 ]
